b68802a9d6ffa88bc1b65a7ba9222c39d30b5cf6
Neetesh Bhardwaj <neebhard@publicisgroupe.net>
2020-03-30 04:37:29 +0530

    fixed naming convention

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,21 +1,19 @@
 requirejs.config({
     baseUrl: './assets/js',
     paths: {
-		lib: './lib',
-		pages: './pages',
-		jquery: './vendor/jquery-1.11.2.min',
-		wayPoint: './vendor/waypoints.min',
-		qrCode: './vendor/easy.qrcode',
-		annyang: './vendor/annyang.min',
-		annyangUI: './vendor/speechkitt.min',
-		botlibreSdk: './vendor/botlibre_sdk',
-		timeAgo: './vendor/timeago.full.min'
+		jquery: 'vendor/jquery-1.11.2.min',
+		wayPoint: 'vendor/waypoints.min',
+		qrCode: 'vendor/easy.qrcode',
+		annyang: 'vendor/annyang.min',
+		annyangUI: 'vendor/speechkitt.min',
+		botlibreSdk: 'vendor/botlibre_sdk',
+		timeAgo: 'vendor/timeago.full.min'
     }
 });
 requirejs([
 	'jquery', 
 	'lib/Theme',
 	'lib/EventTracker',
-	'pages/HomePage',
-	'pages/HelloDocPage'
+	'page/HomePage',
+	'page/HelloDocPage'
 ]);
